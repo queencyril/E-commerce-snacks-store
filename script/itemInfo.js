8e5c9eb822766f63bd5b88eeef003191e10e6f6e
Oluchi Queen <osinachiiheagwara@gmail.com>
2024-06-28 17:03:38 +0100

    random changes

--- script/itemInfo.js
+++ script/itemInfo.js
@@ -5,36 +5,36 @@
     name: "MUNCH iT",
     price: 250,
   },
-  {
-    id: "002",
-    image: "images/digestive-coaster_lgmbqt.png",
-    name: "DIGESTIVE COASTER",
-    price: 400,
-  },
-  {
-    id: "003",
-    image: "images/5.webp",
-    name: "SHRIMP CRACKERS",
-    price: 850,
-  },
-  {
-    id: "004",
-    image: "images/4.png",
-    name: "SPEEDY MINI",
-    price: 430,
-  },
-  {
-    id: "005",
-    image: "images/crispy-peanuts-mockups.png",
-    name: "NUTZY PEANUTS",
-    price: 150,
-  },
-  {
-    id: "006",
-    image: "images/1.png",
-    name: "NA-SO POPCORN",
-    price: 500,
-  },
+  // {
+  //   id: "002",
+  //   image: "images/digestive-coaster_lgmbqt.png",
+  //   name: "DIGESTIVE COASTER",
+  //   price: 400,
+  // },
+  // {
+  //   id: "003",
+  //   image: "images/5.webp",
+  //   name: "SHRIMP CRACKERS",
+  //   price: 850,
+  // },
+  // {
+  //   id: "004",
+  //   image: "images/4.png",
+  //   name: "SPEEDY MINI",
+  //   price: 430,
+  // },
+  // {
+  //   id: "005",
+  //   image: "images/crispy-peanuts-mockups.png",
+  //   name: "NUTZY PEANUTS",
+  //   price: 150,
+  // },
+  // {
+  //   id: "006",
+  //   image: "images/1.png",
+  //   name: "NA-SO POPCORN",
+  //   price: 500,
+  // },
 ];
 
 let itemInfosHTML = '';
